1c93b12c011e7a883ad75f5dc1d56332d91c6d45
Andrew Waite <apwaite86@gmail.com>
2021-03-15 00:29:49 +0000

    Selected elements and stored them in variables. Added event listeners and logic to the number buttons, CE button and Del button

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
+const display = document.querySelector(".display");
+
+const numButtons = document.querySelectorAll(".num");
+
+const opButtons = document.querySelectorAll(".op");
+
+const eqButton = document.querySelector(".equals");
+
+const decButton = document.querySelector(".dec");
+
+const delButton = document.querySelector(".del");
+
+const ceButton = document.querySelector(".ce");
+
+let opOne = "";
+let opTwo = "";
+let currOp = null;
+
+ceButton.addEventListener("click", clearEverything);
+
+delButton.addEventListener("click", delLast);
+
+numButtons.forEach((button) => {
+  button.addEventListener("click", () => appendNum(button.textContent));
+});
+
+opButtons.forEach((button) => {
+  button.addEventListener("click", () => setOp(button.textContent));
+});
+
+function appendNum(num) {
+  if (display.textContent === "0") {
+    clearDisplay();
+  }
+  display.textContent += num;
+}
+
+function clearEverything() {
+  display.textContent = "0";
+  opOne = "";
+  opTwo = "";
+  currOp = null;
+}
+
+function delLast() {
+  display.textContent = display.textContent.slice(0, -1);
+  // if the element has 0 child nodes then the element is empty
+  if (display.childNodes.length === 0) {
+    display.textContent = "0";
+  }
+}
+
+function clearDisplay() {
+  display.textContent = "";
+}
+
 const add = (n1, n2) => {
   return n1 + n2;
 };
@@ -29,7 +85,7 @@ const mod = (n1, n2) => {
 //   return n1 * factorial(n1 - 1);
 // };
 
-const operate = (op, n1, n2) => {
+const operate = (n1, op, n2) => {
   n1 = Number(n1);
   n2 = Number(n2);
   switch (op) {
